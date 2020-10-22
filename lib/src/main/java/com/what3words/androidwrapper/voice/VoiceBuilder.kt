7671860fe3e9cf7d291b09066255b9a321d5c58f
Mani <emanuel@what3words.com>
2020-10-22 17:45:25 +0100

    Added voice support to android wrapper, review readme, gradle artifact upload to ossr

---
+++ lib/src/main/java/com/what3words/androidwrapper/voice/VoiceBuilder.kt
@@ -0,0 +1,267 @@
+package com.what3words.androidwrapper.voice
+
+import android.media.AudioFormat
+import android.media.AudioRecord
+import android.media.MediaRecorder
+import com.what3words.androidwrapper.What3WordsV3
+import com.what3words.androidwrapper.voice.VoiceBuilder.Microphone.Companion.RECORDING_RATE
+import com.what3words.javawrapper.request.BoundingBox
+import com.what3words.javawrapper.request.Coordinates
+import com.what3words.javawrapper.response.Suggestion
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import okhttp3.WebSocket
+import okio.ByteString
+import kotlin.math.abs
+import kotlin.math.log10
+
+class VoiceBuilder(
+    private val api: What3WordsV3,
+    private val mic: Microphone,
+    private val voiceLanguage: String
+) : VoiceApiListener {
+    private var clipToPolygon: Array<Coordinates>? = null
+    private var clipToBoundingBox: BoundingBox? = null
+    private var clipToCircle: Coordinates? = null
+    private var clipToCircleRadius: Double? = null
+    private var clipToCountry: Array<String>? = null
+    private var nFocusResults: Int? = null
+    private var focus: Coordinates? = null
+    private var nResults: Int = 3
+    private var onSuggestionsCallback: ((suggestions: List<Suggestion>) -> Unit)? = null
+    private var onErrorCallback: ((message: String) -> Unit)? = null
+    private var isListening = false
+
+    init {
+        api.voiceApi.listener = this
+    }
+
+    override fun connected(socket: WebSocket) {
+        mic.startRecording(socket)
+    }
+
+    override fun suggestions(suggestions: List<Suggestion>) {
+        mic.stopRecording()
+        isListening = false
+        CoroutineScope(Dispatchers.Main).launch {
+            onSuggestionsCallback?.invoke(suggestions)
+        }
+    }
+
+    override fun error(message: String) {
+        mic.stopRecording()
+        isListening = false
+        CoroutineScope(Dispatchers.Main).launch {
+            onErrorCallback?.invoke(message)
+        }
+    }
+
+    fun onSuggestions(callback: ((suggestions: List<Suggestion>) -> Unit)): VoiceBuilder {
+        this.onSuggestionsCallback = callback
+        return this
+    }
+
+    fun onError(callback: ((message: String) -> Unit)): VoiceBuilder {
+        this.onErrorCallback = callback
+        return this
+    }
+
+    fun startListening(): VoiceBuilder {
+        isListening = true
+        api.voiceApi.open(
+            RECORDING_RATE,
+            url = createSocketUrl()
+        )
+        return this
+    }
+
+    fun isListening(): Boolean {
+        return isListening
+    }
+
+    fun stopListening() {
+        isListening = false
+        mic.stopRecording()
+        api.voiceApi.forceStop()
+    }
+
+    /**
+     * This is a location, specified as a latitude (often where the user making the query is). If specified, the results will be weighted to
+     * give preference to those near the <code>focus</code>. For convenience, longitude is allowed to wrap around the 180 line, so 361 is equivalent to 1.
+     *
+     * @param coordinates the focus to use
+     * @return a {@link W3WAutoSuggestEditText} instance
+     */
+    fun focus(coordinates: Coordinates?): VoiceBuilder {
+        focus = coordinates
+        return this
+    }
+
+    /**
+     * Set the number of AutoSuggest results to return. A maximum of 100 results can be specified, if a number greater than this is requested,
+     * this will be truncated to the maximum. The default is 3
+     *
+     * @param n the number of AutoSuggest results to return
+     * @return a {@link W3WAutoSuggestEditText} instance
+     */
+    fun nResults(n: Int?): VoiceBuilder {
+        nResults = n ?: 3
+        return this
+    }
+
+    /**
+     * Specifies the number of results (must be &lt;= nResults) within the results set which will have a focus. Defaults to <code>nResults</code>.
+     * This allows you to run autosuggest with a mix of focussed and unfocussed results, to give you a "blend" of the two. This is exactly what the old V2
+     * <code>standardblend</code> did, and <code>standardblend</code> behaviour can easily be replicated by passing <code>nFocusResults=1</code>,
+     * which will return just one focussed result and the rest unfocussed.
+     *
+     * @param n number of results within the results set which will have a focus
+     * @return a {@link W3WAutoSuggestEditText} instance
+     */
+    fun nFocusResults(n: Int?): VoiceBuilder {
+        nFocusResults = n
+        return this
+    }
+
+    /**
+     * Restrict autosuggest results to a circle, specified by <code>Coordinates</code> representing the centre of the circle, plus the
+     * <code>radius</code> in kilometres. For convenience, longitude is allowed to wrap around 180 degrees. For example 181 is equivalent to -179.
+     *
+     * @param centre the centre of the circle
+     * @param radius the radius of the circle in kilometres
+     * @return a {@link W3WAutoSuggestEditText} instance
+     */
+    fun clipToCircle(
+        centre: Coordinates?,
+        radius: Double?
+    ): VoiceBuilder {
+        clipToCircle = centre
+        clipToCircleRadius = radius
+        return this
+    }
+
+    /**
+     * Restricts autosuggest to only return results inside the countries specified by comma-separated list of uppercase ISO 3166-1 alpha-2 country codes
+     * (for example, to restrict to Belgium and the UK, use <code>clipToCountry("GB", "BE")</code>. <code>clipToCountry</code> will also accept lowercase
+     * country codes. Entries must be two a-z letters. WARNING: If the two-letter code does not correspond to a country, there is no error: API simply
+     * returns no results.
+     *
+     * @param countryCodes countries to clip results too
+     * @return a {@link W3WAutoSuggestEditText} instance
+     */
+    fun clipToCountry(countryCodes: List<String>): VoiceBuilder {
+        clipToCountry = if (countryCodes.isNotEmpty()) countryCodes.toTypedArray() else null
+        return this
+    }
+
+    /**
+     * Restrict autosuggest results to a <code>BoundingBox</code>.
+     *
+     * @param boundingBox <code>BoundingBox</code> to clip results too
+     * @return a {@link W3WAutoSuggestEditText} instance
+     */
+    fun clipToBoundingBox(
+        boundingBox: BoundingBox?
+    ): VoiceBuilder {
+        clipToBoundingBox = boundingBox
+        return this
+    }
+
+    /**
+     * Restrict autosuggest results to a polygon, specified by a collection of <code>Coordinates</code>. The polygon should be closed,
+     * i.e. the first element should be repeated as the last element; also the list should contain at least 4 entries. The API is currently limited to
+     * accepting up to 25 pairs.
+     *
+     * @param polygon the polygon to clip results too
+     * @return a {@link W3WAutoSuggestEditText} instance
+     */
+    fun clipToPolygon(
+        polygon: List<Coordinates>
+    ): VoiceBuilder {
+        clipToPolygon = if (polygon.isNotEmpty()) polygon.toTypedArray() else null
+        return this
+    }
+
+    private fun createSocketUrl(): String {
+        var url = VoiceApi.BASE_URL
+        url += if (voiceLanguage == "zh") "?voice-language=cmn"
+        else "?voice-language=$voiceLanguage"
+        url += "&n-results=$nResults"
+        focus?.let {
+            url += "&focus=${focus!!.lat},${focus!!.lng}"
+            if (nFocusResults != null) {
+                url += "&n-focus-results=$nFocusResults"
+            }
+        }
+        clipToCountry?.let {
+            url += "&clip-to-country=${it.joinToString()}"
+        }
+        clipToCircle?.let {
+            url += "&clip-to-circle=${it.lat},${it.lng},${clipToCircleRadius ?: 1}"
+        }
+        clipToPolygon?.let {
+            url += "&clip-to-polygon=${it.joinToString()}"
+        }
+        clipToBoundingBox?.let {
+            url += "&clip-to-polygon=${it.sw.lat},${it.sw.lng},${it.ne.lat},${it.ne.lng}"
+        }
+        return url
+    }
+
+    class Microphone() {
+        companion object {
+            const val RECORDING_RATE = 44100
+            const val CHANNEL = AudioFormat.CHANNEL_IN_MONO
+            const val FORMAT = AudioFormat.ENCODING_PCM_16BIT
+        }
+
+        private var onListeningCallback: ((dbValue: Float?) -> Unit)? = null
+
+        private val bufferSize = AudioRecord.getMinBufferSize(
+            RECORDING_RATE, CHANNEL, FORMAT
+        )
+
+        private var recorder: AudioRecord? = null
+        private var continueRecording: Boolean = false
+
+        fun onListening(callback: (dbValue: Float?) -> Unit): Microphone {
+            this.onListeningCallback = callback
+            return this
+        }
+
+        internal fun stopRecording() {
+            continueRecording = false
+            recorder?.release()
+        }
+
+        internal fun startRecording(socket: WebSocket) {
+            recorder = AudioRecord(
+                MediaRecorder.AudioSource.MIC,
+                RECORDING_RATE,
+                CHANNEL,
+                FORMAT,
+                bufferSize
+            ).also { audioRecord ->
+                continueRecording = true
+                CoroutineScope(Dispatchers.IO).launch {
+                    val buffer = ByteArray(bufferSize)
+                    var oldTimestamp = System.currentTimeMillis()
+                    audioRecord.startRecording()
+                    while (continueRecording) {
+                        audioRecord.read(buffer, 0, buffer.size)
+                        if ((System.currentTimeMillis() - oldTimestamp) > 100) {
+                            oldTimestamp = System.currentTimeMillis()
+                            val dB =
+                                VoiceSignalParser.transform(buffer.map { abs(it.toDouble()) }.sum())
+                            CoroutineScope(Dispatchers.Main).launch {
+                                onListeningCallback?.invoke(dB)
+                            }
+                        }
+                        socket.send(ByteString.of(*buffer))
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ sample/src/main/java/com/what3words/androidwrappersample/MainActivity.kt
@@ -0,0 +1,141 @@
+package com.what3words.androidwrappersample
+
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.PermissionChecker
+import com.what3words.androidwrapper.What3WordsV3
+import com.what3words.androidwrapper.voice.VoiceBuilder
+import com.what3words.javawrapper.request.Coordinates
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlin.math.roundToInt
+
+class MainActivity : AppCompatActivity() {
+
+    private var builder: VoiceBuilder? = null
+    private val wrapper by lazy {
+        What3WordsV3("YOUR_API_KEY_HERE", this)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //convert-to-3wa sample
+        buttonConvertTo3wa.setOnClickListener {
+            val latLong = textInputConvertTo3wa.text?.replace("\\s".toRegex(), "")?.split(",")
+                ?.filter { it.isNotEmpty() }
+            val lat = latLong?.getOrNull(0)?.toDoubleOrNull()
+            val long = latLong?.getOrNull(1)?.toDoubleOrNull()
+            if (lat != null && long != null) {
+                CoroutineScope(Dispatchers.IO).launch {
+                    //use wrapper.convertTo3wa() with Dispatcher.IO - background thread
+                    val result = wrapper.convertTo3wa(Coordinates(lat, long)).execute()
+                    CoroutineScope(Dispatchers.Main).launch {
+                        //use Dispatcher.Main to update your views with the results - Main thread
+                        if (result.isSuccessful) {
+                            resultConvertTo3wa.text = "3 word address: ${result.words}"
+                        } else {
+                            resultConvertTo3wa.text = result.error.message
+                        }
+                    }
+                }
+            } else {
+                resultConvertTo3wa.text = "invalid lat,long"
+            }
+        }
+
+        //convert-to-coordinates sample
+        buttonConvertToCoordinates.setOnClickListener {
+            CoroutineScope(Dispatchers.IO).launch {
+                //use wrapper.convertToCoordinates() with Dispatcher.IO - background thread
+                val result =
+                    wrapper.convertToCoordinates(textInputConvertToCoordinates.text.toString())
+                        .execute()
+                CoroutineScope(Dispatchers.Main).launch {
+                    //use Dispatcher.Main to update your views with the results - Main thread
+                    if (result.isSuccessful) {
+                        resultConvertToCoordinates.text =
+                            "Coordinates: ${result.coordinates.lat}, ${result.coordinates.lng}"
+                    } else {
+                        resultConvertToCoordinates.text = result.error.message
+                    }
+                }
+            }
+        }
+
+        //text autosuggest sample
+        buttonAutoSuggest.setOnClickListener {
+            CoroutineScope(Dispatchers.IO).launch {
+                //use wrapper.autosuggest() with Dispatcher.IO - background thread
+                val result = wrapper.autosuggest(textInputAutoSuggest.text.toString()).execute()
+                CoroutineScope(Dispatchers.Main).launch {
+                    //use Dispatcher.Main to update your views with the results - Main thread
+                    if (result.isSuccessful) {
+                        resultAutoSuggest.text = if (result.suggestions.count() != 0)
+                            "Suggestions: ${result.suggestions.joinToString { it.words }}"
+                        else "No suggestions available"
+                    } else {
+                        resultAutoSuggest.text = result.error.message
+                    }
+                }
+            }
+        }
+
+        //voice autosuggest sample
+        builder = wrapper.autosuggest(VoiceBuilder.Microphone().onListening {
+            it?.let { volume ->
+                volumeAutoSuggestVoice.text =
+                    "volume: ${(volume.times(100).roundToInt())}"
+            }
+        }, "en")
+            .onSuggestions { suggestions ->
+                buttonAutoSuggestVoice.setIconResource(R.drawable.ic_record)
+                resultAutoSuggestVoice.text =
+                    "Suggestions: ${suggestions.joinToString { it.words }}"
+            }.onError { error ->
+                buttonAutoSuggestVoice.setIconResource(R.drawable.ic_record)
+                resultAutoSuggestVoice.text = error
+            }
+
+        buttonAutoSuggestVoice.setOnClickListener {
+            if (builder?.isListening() == true) {
+                buttonAutoSuggestVoice.setIconResource(R.drawable.ic_record)
+                builder?.stopListening()
+            } else {
+                val permission =
+                    PermissionChecker.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO)
+
+                if (permission == PermissionChecker.PERMISSION_GRANTED) {
+                    buttonAutoSuggestVoice.setIconResource(R.drawable.ic_stop)
+                    builder?.startListening()
+                } else {
+                    ActivityCompat.requestPermissions(
+                        this,
+                        arrayOf(Manifest.permission.RECORD_AUDIO),
+                        1
+                    )
+                }
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == 1 && grantResults.count() == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            buttonAutoSuggestVoice.setIconResource(R.drawable.ic_stop)
+            builder?.startListening()
+        } else {
+            resultAutoSuggestVoice.text = "record audio permission required"
+        }
+    }
+}
